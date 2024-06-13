24b3896f7398037543e8b951dc628bcfd798ac78
jinsek <jinsek@cs.washington.edu>
2024-06-12 22:53:22 -0700

    changes from desktop

--- public/index.js
+++ public/index.js
@@ -1,7 +1,5 @@
 /**
  * Name: Jinseok Kim
- * Date: May 15, 2024
- * Section: AC
  * This file represents a simple game where a character jumps over obstacles.
  * It includes functions such as handling character jumps, moving obstacles, starting a timer,
  * checking for collisions, a
